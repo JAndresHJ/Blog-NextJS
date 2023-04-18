8e8689e4495d0a1e8a24c74ccfc4aac574833648
Andres <jandreshdzj@gmail.com>
2023-04-18 12:37:34 -0500

    Enhaces post content using SyntaxHighlighter for code snippets

--- components/posts/PostDetail/PostContent.tsx
+++ components/posts/PostDetail/PostContent.tsx
@@ -3,14 +3,53 @@ import PostHeader from './PostHeader';
 import classes from './postContent.module.css';
 import { FC } from 'react';
 import { PostDetailProps } from '@/pages/posts/[slug]';
+import { Components } from 'react-markdown';
+import Image from 'next/image';
+import { Element } from 'react-markdown/lib/ast-to-react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const PostContent: FC<PostDetailProps> = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const components: Components = {
+    code: (code) => {
+      const classNameSplitted = code.className?.split('-');
+      const codeStr = code.children[0] as string;
+      const language = classNameSplitted![1];
+
+      return (
+        <SyntaxHighlighter style={coldarkDark} language={language}>
+          {codeStr}
+        </SyntaxHighlighter>
+      );
+    },
+    p: (paragraph) => {
+      const { node } = paragraph;
+      const child = node.children[0] as Element;
+      if (child?.tagName === 'img') {
+        const img = node.children[0] as Element;
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${img.properties!.src}`}
+              alt={img.properties!.alt as string}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={components}>{post.content}</ReactMarkdown>
     </article>
   );
 };
